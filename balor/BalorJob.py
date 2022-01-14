c17506a2219fddcca365892fda2f962fd9022ae8
Tatarize <tatarize@gmail.com>
2022-01-14 04:58:14 -0800

    Correct Jobs API

--- balor/BalorJob.py
+++ balor/BalorJob.py
@@ -378,104 +378,34 @@ def OperationFactory(code, tracking=None, position=0):
     OpClass = operations_by_opcode.get(opcode, Operation)
     return OpClass(from_binary=code, tracking=tracking, position=position)
 
+
 class Job:
-    def __init__(self, machine=None):
+    def __init__(self, x=0x8000, y=0x8000, machine=None):
         self.machine = machine
-        self.x_scale = 1
-        self.y_scale = 1
-        self.scale_unit = ""
         self.operations = []
+        self._last_x = self._last_x
+        self._last_y = self._last_y
+        self._start_x = self._start_x
+        self._start_y = self._start_y
+        self._ready = False
+        self._cut_speed = None
+        self._travel_speed = None
+        self._frequency = None
+        self._power = None
+        self._laser_control = None
+        self._laser_on_delay = None
+        self._laser_off_delay = None
+        self._poly_delay = None
+        self._markend_delay = None
+        self._light = None
 
-    def get_scale(self):
-        return self.x_scale, self.y_scale, self.scale_unit
-
-    def clear_operations(self):
-        self.operations = []
-
-    def get_position(self):
+    @property
+    def position(self):
         return len(self.operations) - 1
 
     def duplicate(self, begin, end, repeats=1):
         for _ in range(repeats):
             self.operations.extend(self.operations[begin:end])
-
-    def __iter__(self):
-        return iter(self.operations)
-
-    def add_light_prefix(self, travel_speed):
-        self.extend(
-            [
-                OpReadyMark(),
-                OpJumpSpeed(travel_speed),
-                # OpMystery0D(0x0008)
-            ]
-        )
-
-    def line(self, x0, y0, x1, y1, seg_size=5, Op=OpMarkTo):
-        length = ((x0 - x1) ** 2 + (y0 - y1) ** 2) ** 0.5
-        segs = max(2, int(round(length / seg_size)))
-        #print ("**", x0, y0, x1, y1, length, segs, file=sys.stderr)
-
-        xs = np.linspace(x0, x1, segs)
-        ys = np.linspace(y0, y1, segs)
-
-        for n in range(segs):
-            #print ("*", xs[n], ys[n], self.cal.interpolate(xs[n], ys[n]), file=sys.stderr)
-            self.append(Op(*self.cal.interpolate(xs[n], ys[n])))
-
-    def change_settings(self, q_switch_period, laser_power, cut_speed):
-        self.extend(
-            [
-                OpSetQSwitchPeriod(q_switch_period),
-                MarkPowerRatio(laser_power),
-                OpMarkSpeed(cut_speed),
-                # OpMystery0D(0x0008),
-            ]
-        )
-
-    # self.settings[color] = (q_switch_period, laser_power, cut_speed, hatch_angle,
-    #                hatch_spacing, hatch_pattern, repeats)
-
-    def add_mark_prefix(self, travel_speed, q_switch_period, laser_power, cut_speed):
-        self.extend(
-            [
-                OpReadyMark(),
-                OpSetQSwitchPeriod(q_switch_period),
-                MarkPowerRatio(laser_power),
-                OpJumpSpeed(travel_speed),
-                OpMarkSpeed(cut_speed),
-                OpLaserOnDelay(0x0064, 0x8000),
-                OpLaserOffDelay(0x0064),
-                OpPolygonDelay(0x000A),
-                # OpMystery0D(0x0008),
-            ]
-        )
-
-    def laser_control(self, on):
-        if on:
-            self.extend(
-                [
-                    OpLaserControl(0x0001),
-                    OpMarkEndDelay(0x0320),
-                ]
-            )
-        else:
-            self.extend(
-                [
-                    OpMarkEndDelay(0x001E),
-                    OpLaserControl(0x0000),
-                ]
-            )
-
-    def plot(self, draw, resolution=2048):
-        sim = Simulation(self, self.machine, draw, resolution)
-        for op in self.operations:
-            sim.simulate(op)
-
-    def set_scale(self, x=1, y=1, unit=""):
-        self.x_scale = x
-        self.y_scale = y
-        self.scale_unit = unit
 
     def append(self, x):
         x.bind(self)
@@ -486,34 +416,11 @@ class Job:
             op.bind(self)
         self.operations.extend(x)
 
-    def set_travel_speed(self, speed):
-        converted = int(round(speed / 2.0))  # units are 2mm/sec
-        self.append(OpJumpSpeed(converted))
+    def __iter__(self):
+        return iter(self.operations)
 
-    def set_cut_speed(self, speed):
-        converted = int(round(speed / 2.0))
-        self.append(OpMarkSpeed(converted))
-
-    def set_laser_power(self, power):
-        converted = int(round(power * 40.95))
-        self.append(MarkPowerRatio(converted))
-
-    def set_q_switch_period(self, frequency):
-        q_switch_period = int(round(1.0 / (frequency * 1e3) / 50e-9))
-        self.append(OpSetQSwitchPeriod(q_switch_period))
-
-    def get_operations(self):
-        return self.operations
-
-    def add_packet(self, data, tracking=None):
-        # Parse MSBF data and add it as operations
-        i = 0
-        while i < len(data):
-            command = data[i : i + 12]
-            op = OperationFactory(command, tracking=tracking, position=i)
-            op.bind(self)
-            self.operations.append(op)
-            i += 12
+    def __bytes__(self):
+        return self.serialize()
 
     def serialize(self):
         """
@@ -521,17 +428,8 @@ class Job:
 
         :return:
         """
-        self._calculate_distances()
-        size = 256 * int(round(math.ceil(len(self.operations) / 256.0)))
-        buf = bytearray(([0x02, 0x80] + [0] * 10) * size)  # Create buffer full of NOP
-        i = 0
-        for op in self.operations:
-            buf[i : i + 12] = op.serialize()
-            i += 12
-        return buf
-
-    def _calculate_distances(self):
-        last_xy = 0x8000, 0x8000
+        # Calculate distances.
+        last_xy = start_x, start_y
         for op in self.operations:
             if op.has_d():
                 nx, ny = op.get_xy()
@@ -541,12 +439,264 @@ class Job:
             if op.has_xy():
                 last_xy = op.get_xy()
 
+        # Write buffer.
+        size = 256 * int(round(math.ceil(len(self.operations) / 256.0)))
+        buf = bytearray(([0x02, 0x80] + [0] * 10) * size)  # Create buffer full of NOP
+        i = 0
+        for op in self.operations:
+            buf[i : i + 12] = op.serialize()
+            i += 12
+        return buf
+
+    ######################
+    # GEOMETRY HELPERS
+    ######################
+
+    def draw_line(self, x0, y0, x1, y1, seg_size=5, Op=OpMarkTo):
+        length = ((x0 - x1) ** 2 + (y0 - y1) ** 2) ** 0.5
+        segs = max(2, int(round(length / seg_size)))
+        # print ("**", x0, y0, x1, y1, length, segs, file=sys.stderr)
+
+        xs = np.linspace(x0, x1, segs)
+        ys = np.linspace(y0, y1, segs)
+
+        for n in range(segs):
+            # print ("*", xs[n], ys[n], self.cal.interpolate(xs[n], ys[n]), file=sys.stderr)
+            self.append(Op(*self.pos(xs[n], ys[n])))
+
+    ######################
+    # UNIT CONVERSION
+    ######################
+
+    def pos(self, x, y):
+        if self.cal is None:
+            return x, y
+        return self.cal.interpolate(x, y)
+
+    def convert_time(self, time):
+        # TODO: WEAK IMPLEMENTATION
+        raise NotImplementedError("No time units")
+
+    def convert_speed(self, speed):
+        return int(round(speed / 2.0))  # units are 2mm/sec
+
+    def convert_power(self, power):
+        return int(round(power * 40.95))
+
+    def convert_frequency(self, frequency):
+        # q_switch_period
+        return int(round(1.0 / (frequency * 1e3) / 50e-9))
+
+    ######################
+    # COMMAND DELEGATES
+    ######################
+
+    def ready(self):
+        """
+        Flag this job with ReadyMark.
+
+        :return:
+        """
+        if not self._ready:
+            self._ready = True
+            self.append(OpReadyMark)
+
+    def laser_control(self, control):
+        """
+        Enable the laser control.
+
+        :param control:
+        :return:
+        """
+        if self._laser_control == control:
+            return
+        self._laser_control = control
+
+        # TODO: Does this order matter?
+        if control:
+            self.append(OpLaserControl(0x0001))
+            self.markend_delay(0x0320)
+        else:
+            self.markend_delay(0x001E)
+            self.append(OpLaserControl(0x0000))
+
+    def set_travel_speed(self, speed):
+        if self._travel_speed == speed:
+            return
+        self.ready()
+        self._travel_speed = speed
+        self.append(OpJumpSpeed(self.convert_speed(speed)))
+
+    def set_cut_speed(self, speed):
+        if self._cut_speed == speed:
+            return
+        self.ready()
+        self._cut_speed = speed
+        self.append(OpMarkSpeed(self.convert_speed(speed)))
+
+    def set_power(self, power):
+        if self._power == power:
+            return
+        self.ready()
+        self._power = power
+        self.append(MarkPowerRatio(self.convert_power(power)))
+
+    def set_frequency(self, frequency):
+        if self._qswitch == frequency:
+            return
+        self._qswitch = frequency
+        # if self.machine == "something":
+        # TODO: use differs by machine: 0x800A Mark Frequency, 0x800B Mark Pulse Width
+        self.append(OpSetQSwitchPeriod(self.convert_frequency(frequency)))
+
+    def set_light(self, on):
+        # TODO: WEAK IMPLEMENTATION
+        if self._light == on:
+            return
+        # In theory WriterPort(0x100) maybe should turn the light on if it isn't on.
+        # More research needed.
+        pass
+
+    def laser_on_delay(self, delay="dummy"):
+        # TODO: WEAK IMPLEMENTATION
+        if self._laser_on_delay == delay:
+            return
+        self.ready()
+        self._laser_on_delay = delay
+        self.append(OpLaserOnDelay(0x0064, 0x8000))
+
+    def laser_off_delay(self, delay="dummy"):
+        # TODO: WEAK IMPLEMENTATION
+        if self._laser_off_delay == delay:
+            return
+        self.ready()
+        self._laser_off_delay = delay
+        self.append(OpLaserOffDelay(0x0064))
+
+    def polygon_delay(self, delay="dummy"):
+        # TODO: WEAK IMPLEMENTATION
+        if self._poly_delay == delay:
+            return
+        self.ready()
+        self._poly_delay = delay
+        self.append(OpPolygonDelay(0x000A))
+
+    def markend_delay(self, delay):
+        # TODO: WEAK IMPLEMENTATION
+        if self._markend_delay == delay:
+            return
+        self.append(OpMarkEndDelay(delay))
+
+    def curve_calibration(self, curve="dummy"):
+        # TODO: WEAK IMPLEMENTATION
+        if self._curve_calibration == curve:
+            return
+        self.ready()
+        self._curve_calibration = curve
+        # self.append(OpAltTravel(0x0008))
+
+    def mark(self, x, y):
+        """
+        Mark to a new location with the laser firing.
+
+        :param x:
+        :param y:
+        :return:
+        """
+        self.ready()
+        if self._qswitch is None:
+            raise ValueError("Qswitch frequency must be set before a mark(x,y)")
+        if self._power is None:
+            raise ValueError("Laser Power must be set before a mark(x,y)")
+        if self._mark_speed is None:
+            raise ValueError("Mark Speed must be set before a mark(x,y)")
+        if self._laser_on_delay is None:
+            # raise ValueError("LaserOn Delay must be set before a mark(x,y)")
+            self.laser_on_delay()
+        if self._laser_off_delay is None:
+            # raise ValueError("LaserOff Delay must be set before a mark(x,y)")
+            self.laser_off_delay()
+        if self._poly_delay is None:
+            # raise ValueError("Polygon Delay must be set before a mark(x,y)")
+            self.poly_delay()
+        if self._curve_calibration is None:
+            self.curve_calibration()
+        self._last_x = x
+        self._last_y = y
+        self.append(balor.BalorJob.OpMarkTo(*self.pos(x, y)))
+
+    def light(self, x, y):
+        """
+        Move to a new location with light enabled.
+
+        :param x:
+        :param y:
+        :return:
+        """
+        self.ready()
+        if not self._travel_speed:
+            raise ValueError("Travel speed must be set before a light(x,y)")
+        if not self._light:
+            self.set_light(True)
+        self._last_x = x
+        self._last_y = y
+        self.append(balor.BalorJob.OpJumpTo(*self.pos(x, y)))
+
+    def goto(self, x, y):
+        """
+        Move to a new location without laser or light.
+
+        :param x:
+        :param y:
+        :return:
+        """
+        if not self._ready:
+            self.ready()
+        if not self._travel_speed:
+            raise ValueError("Travel speed must be set before a goto(x,y)")
+        self._last_x = x
+        self._last_y = y
+        self.append(balor.BalorJob.OpJumpTo(*self.pos(x, y)))
+
+    def init(self, x, y):
+        """
+        Sets the initial position. This is the position we came from to get to this set of operations. It matters for
+        the time calculation to the initial goto or mark commands.
+
+        :param x:
+        :param y:
+        :return:
+        """
+        self._last_x = x
+        self._last_y = y
+        self._start_x = x
+        self._start_y = y
+
+    ######################
+    # COMMAND DELEGATES
+    ######################
+
+    def add_packet(self, data, tracking=None):
+        """
+        Parse MSBF data and add it as operations
+
+        :param data:
+        :param tracking:
+        :return:
+        """
+        i = 0
+        while i < len(data):
+            command = data[i : i + 12]
+            op = OperationFactory(command, tracking=tracking, position=i)
+            op.bind(self)
+            self.operations.append(op)
+            i += 12
+
+    def plot(self, draw, resolution=2048):
+        sim = Simulation(self, self.machine, draw, resolution)
+        for op in self.operations:
+            sim.simulate(op)
+
     def serialize_to_file(self, file):
         with open(file, "wb") as out_file:
             out_file.write(self.serialize())
-
-
-def JobFactory(machine_name):
-    # This is currently just a stub since we don't support any
-    # incompatible machines
-    return Job(machine=machine_name)
