a640e77f6a339da6212ef41eaffbaa4d28b2d1d2
Tatarize <tatarize@gmail.com>
2022-01-15 09:38:16 -0800

    Add in animated square code

---
+++ drawanimatedsquare.py
@@ -0,0 +1,26 @@
+from balor.sender import Sender
+from math import sin
+
+
+sender = Sender()
+
+sender.open()
+sender.set_xy(0x9000, 0x9000)
+
+
+def tick(cmds, loop_index):
+    cmds.clear()
+    x = int(round(20 * sin(loop_index * 0.1)))
+    job.set_travel_speed(2000)
+    cmds.light(0x9000 + (x * 1000), 0x7000 + (x * 1000))
+    cmds.light(0x7000 + (x * 1000), 0x7000 + (x * 1000))
+    cmds.light(0x7000 + (x * 1000), 0x9000 + (x * 1000))
+    cmds.light(0x9000 + (x * 1000), 0x9000 + (x * 1000))
+    print(loop_index)
+
+
+job = sender.job(tick=tick)
+job.execute(1000)
+sender.close()
+
+
