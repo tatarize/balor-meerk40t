00d712cef46f042ec97150fa54098d6411f9b99f
Tatarize <tatarize@gmail.com>
2022-01-16 11:05:04 -0800

    Delete a bunch of stuff that is balor specific

--- drawanimatedsquare.py
+++ drawanimatedsquare.py
@@ -11,7 +11,7 @@ sender.set_xy(0x9000, 0x9000)
 def tick(cmds, loop_index):
     cmds.clear()
     x = int(round(20 * sin(loop_index * 0.1)))
-    job.set_travel_speed(2000)
+    cmds.set_travel_speed(2000)
     cmds.light(0x9000 + (x * 1000), 0x7000 + (x * 1000))
     cmds.light(0x7000 + (x * 1000), 0x7000 + (x * 1000))
     cmds.light(0x7000 + (x * 1000), 0x9000 + (x * 1000))
@@ -22,5 +22,3 @@ def tick(cmds, loop_index):
 job = sender.job(tick=tick)
 job.execute(1000)
 sender.close()
-
-
--- drawsquare.py
+++ drawsquare.py
@@ -1,17 +1,14 @@
 from balor.sender import Sender
 
-# sender = Sender(debug=print, mock=True)
 sender = Sender()
 
 sender.open()
 sender.set_xy(0x9000, 0x9000)
 job = sender.job()
 job.set_travel_speed(2000)
-job.light(0x9000,0x7000)
-job.light(0x7000,0x7000)
-job.light(0x7000,0x9000)
+job.light(0x9000, 0x7000)
+job.light(0x7000, 0x7000)
+job.light(0x7000, 0x9000)
 job.light(0x9000, 0x900)
 job.execute(1000)
 sender.close()
-
-
